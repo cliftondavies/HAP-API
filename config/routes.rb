9bc969451b7ca4f7e9dd6a7ff76273be12389348
cliftondavies <clifton.davies@outlook.com>
2021-03-26 20:56:40 +0000

    Config routes for hikes and appointments resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+
+  resources :hikes, only: %i[index show] do
+    resources :appointments, only: [:create]
+  end
+
+  resources :appointments, path: 'bookings', only: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
